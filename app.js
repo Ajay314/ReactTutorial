975fe5f61e4976f647e6ea7a6e46028f1a3eead7
Ajay314 <109684456+Ajay314@users.noreply.github.com>
2024-11-23 11:58:42 +0530

    Understanding React Elements

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{},"Hello world from React");
+const heading = React.createElement(
+    "h1",
+    {"id":"heading"},
+    "Hello world"
+)
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(heading);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+ const jsxHeading = <h1>Helloo World!!</h1>
+ root.render(jsxHeading);
